3d48a2d0b22202fabc8d66e2cfdd011fc57b96dc
PatillaCode <patillacode@gmail.com>
2024-01-27 04:55:01 +0100

    stable

---
+++ scripts/setup_autoenv.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+setup_autoenv() {
+    project_path=$1
+    echo "echo \"󰚩  loading venv automatically 󰚩\"" > $project_path/.autoenv.sh
+    echo -n "source ${project_path}/venv/bin/activate" >> $project_path/.autoenv.sh
+    echo "echo \"󰚩  deactivating venv automatically 󰚩\"" > $project_path/.autoenv_leave.sh
+    echo -n "if type deactivate >/dev/null 2>&1; then\n  deactivate\nfi" >> $project_path/.autoenv_leave.sh
+    echo ".autoenv.sh" >> $project_path/.gitignore
+    echo ".autoenv_leave.sh" >> $project_path/.gitignore
+    echo "󰚩  autoenv configured 󰚩"
+}
+
+# get the first argument passed to the script as the project_path and call setup_autoenv
+project_path=$1
+setup_autoenv $project_path
